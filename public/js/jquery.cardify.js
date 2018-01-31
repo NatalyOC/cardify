04e87f827458786b64c18c296f18e99ef63d08f4
NatalyOC <noteroc@hotmail.com>
2018-01-31 01:45:15 -0500

    agregando funcionalidad

--- public/js/jquery.cardify.js
+++ public/js/jquery.cardify.js
@@ -4,7 +4,14 @@
 	    $('img').each(function() {       	    	           
         $(this).wrap('<figure></figure>');
         $(this).after('<figcaption class="style-img">' + $(this).attr('alt') + '</figcaption>');       
-	    });  
+      });  
+      $('figure').hover(function() {
+        console.log($(this));
+        $('.style-img',this).slideToggle('slow');
+      }, function() {
+        $('.style-img',this).slideToggle('slow');
+        
+      });
 	  }  
   });
 })(jQuery);
