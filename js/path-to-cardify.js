a6502fa49ad55b146813ac001781e8d152b191c9
NatalyOC <noteroc@hotmail.com>
2018-01-31 01:31:44 -0500

    agregando evento hover

--- js/path-to-cardify.js
+++ js/path-to-cardify.js
@@ -7,18 +7,11 @@
         let replace = `<figure class="image-frame"><img src=${pathSrc}><figcaption class="image-caption"><h5>${pathAlt}</h5></figcaption></figure>`;
         $(this).replaceWith(replace);
       });
-      
-      /*    $('img').hover(function(event) {
-        console.log(event);
-        $(this).stop().animate({opacity: 0.4}, 200);
-      }, function() {
-        $(this).stop().animate({opacity: 1}, 500);
-      }); */
       $('.image-frame').hover(function() {
         console.log($(this));
         $('.image-caption',this).slideToggle('slow');
       }, function() {
-         $('.image-caption',this).slideToggle('slow');
+        $('.image-caption',this).slideToggle('slow');
         
       });
     }
