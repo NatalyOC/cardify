743250780c4750d250c982ce771ad9192aecf529
NatalyOC <noteroc@hotmail.com>
2018-01-31 01:31:37 -0500

    agregando evento hover

--- js/path-to-cardify.js
+++ js/path-to-cardify.js
@@ -4,9 +4,22 @@
       $('img').each(function() {
         let pathSrc = $(this).attr('src');
         let pathAlt = $(this).attr('alt');
-        let replace = `<figure><img src=${pathSrc}><figcaption>${pathAlt}</figcaption></figure>`;
-        // $(this).html(replace);
+        let replace = `<figure class="image-frame"><img src=${pathSrc}><figcaption class="image-caption"><h5>${pathAlt}</h5></figcaption></figure>`;
         $(this).replaceWith(replace);
+      });
+      
+      /*    $('img').hover(function(event) {
+        console.log(event);
+        $(this).stop().animate({opacity: 0.4}, 200);
+      }, function() {
+        $(this).stop().animate({opacity: 1}, 500);
+      }); */
+      $('.image-frame').hover(function() {
+        console.log($(this));
+        $('.image-caption',this).slideToggle('slow');
+      }, function() {
+         $('.image-caption',this).slideToggle('slow');
+        
       });
     }
   });       
